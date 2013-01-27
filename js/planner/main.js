bdd08003e3c9c19de7581de455dd939f2dd74225
Benjamin Wheeler <bwheeler@webassign.net>
2013-01-27 17:30:18 -0500

    Removed spotlight as it was unused

--- js/planner/main.js
+++ js/planner/main.js
@@ -5,11 +5,8 @@ require(['../config'],
         'planner/router',
         'Backbone',
         'jQueryUI',
-        'Spotlight',
         'Bootstrap'
     ], function($, Router, Backbone) {
-        // pretend we have opacity support so that spotlights will appear even in IE8
-        $.support.opacity = true;
         // this is a fix for bootstrap dropdowns on touch devices
         // https://github.com/twitter/bootstrap/issues/2975#issuecomment-6659992
         $('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
